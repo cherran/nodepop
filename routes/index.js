7c832637809ef481f7bb56c2d8cfcb36c1a1ccfb
Carlos Herrán <cherranm@gmail.com>
2017-12-17 23:32:43 +0100

    Fixed linting problems

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const i18n = require('i18n');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.end(res.__('WELCOME'));
 });
 
